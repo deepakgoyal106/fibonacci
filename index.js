3b50aee57b03dc41cdc0f233a6b775bfbf6c8356
deepakgoyal106 <49240693+deepakgoyal106@users.noreply.github.com>
2019-07-05 14:21:15 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 //Fibonacci Function
 function fibSeries(x){
-  var num1 = 0;
-  var num2 = 1;
-  
-  var result = num2;
-  
-  for(var i = 0; i < x; i++){
-    var numTotal = num1 +num2;
-    if(numTotal < x)result += ', ' + numTotal;
-    else break;
-    num1 = num2;
-    num2 = numTotal;
+  if(x > 1){ //option if condition 
+    var num1 = 0;
+    var num2 = 1;
+    
+    var result = num2;
+    
+    for(var i = 0; i < x; i++){
+      var numTotal = num1 +num2;
+      if(numTotal < x)result += ', ' + numTotal;
+      else break;
+      num1 = num2;
+      num2 = numTotal;
+    }
+    console.log(result);
   }
-  
-  console.log(result);
 }
-fibSeries(10);
+fibSeries(3);
